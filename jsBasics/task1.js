49ea47e6e7c9b9977e1b79456e2f08f3be7338c3
Maryna-Olefirenko <molefirenko74@gmail.com>
2024-06-29 13:58:08 +0300

    task1/1

---
+++ jsBasics/task1.js
@@ -0,0 +1,21 @@
+//srting
+let name1 = 'Maryna';
+let str1 = `Hello ${name1}`;
+console.log (str1);
+
+//number
+let number1 = 2;
+let number2 = 22;
+console.log(number1*number2);
+
+//boolean
+const hasLight = true;
+console.log(hasLight);
+
+//null
+let value = null;
+console.log(value);
+
+//undefined
+let valueU;
+console.log(valueU);
