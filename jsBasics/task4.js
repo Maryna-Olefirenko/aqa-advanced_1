d28f948def7832a1739c2ac496f7bfa6f26b17d7
Maryna-Olefirenko <molefirenko74@gmail.com>
2024-06-29 13:59:18 +0300

    task4/1

---
+++ jsBasics/task4.js
@@ -0,0 +1,16 @@
+//4.1
+let radius = 15
+let pl = Math.PI*Math.pow(radius,2);
+console.log((pl).toFixed(2));
+
+
+//4.2
+let lenght = 5
+let width = 10
+console.log ((lenght*width).toFixed(2));
+
+//4.3
+let radius1 = 2
+let height = 5
+let ob = ((Math.PI*Math.pow(radius1,2)*height). toFixed(2));
+console.log (ob);
