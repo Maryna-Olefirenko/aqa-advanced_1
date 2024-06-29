e94ee4c098c1776561c1194727685adcf55f2228
Maryna-Olefirenko <molefirenko74@gmail.com>
2024-06-29 13:58:40 +0300

    task2/1

---
+++ jsBasics/task2.js
@@ -0,0 +1,6 @@
+let name1 = 'Jane'
+let name2 = 'Alice'
+let greetingK = 'Hello ' +name1+' and '+name2
+console.log (greetingK)
+let greeting = `Dear ${name1} and ${name2}, we will glad to see you in Kyiv at our Event!`;
+console.log(greeting)
